ea1a9c3c3843d3e2b93f93ea2f179343baae1540
Kevin Bell <bellKevin@pm.me>
2022-06-16 20:57:53 +0000

    Add files via upload

---
+++ milesPerGallonDriver/src/milespergallondriver/MilesPerGallonDriver.java
@@ -0,0 +1,9 @@
+package milespergallondriver;
+
+public class MilesPerGallonDriver {
+
+    public static void main(String[] args) {
+         System.out.println("Ch 7 MPG Project by Kevin Bell\n");
+         MPG milesPerGallon= new MPG();
+    }
+}
