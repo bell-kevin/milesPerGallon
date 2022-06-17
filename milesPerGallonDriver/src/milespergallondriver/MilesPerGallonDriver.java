269723cfe9f58df85b6866a67385056a4d320bc0
Kevin Bell <bellKevin@pm.me>
2022-06-17 00:44:02 +0000

    Update MilesPerGallonDriver.java

--- milesPerGallonDriver/src/milespergallondriver/MilesPerGallonDriver.java
+++ milesPerGallonDriver/src/milespergallondriver/MilesPerGallonDriver.java
@@ -1,9 +1,17 @@
 package milespergallondriver;
 
-public class MilesPerGallonDriver {
+public class MilesPerGallon {
 
     public static void main(String[] args) {
-         System.out.println("Ch 7 MPG Project by Kevin Bell\n");
-         MPG milesPerGallon= new MPG();
+        System.out.println("Ch 7 MPG Project by Kevin Bell\n");
+        MPG trip1 = new MPG(320.0, 29.0);
+        MPG trip2 = new MPG(325.8, 32.1);
+        MPG trip3 = new MPG(412.5, 35);
+        MPG trip4 = new MPG(345, 32.6);
+        trip1.displayCurrentMPG();
+        trip2.displayCurrentMPG();
+        trip3.displayCurrentMPG();
+        trip4.displayCurrentMPG();
+        MPG.displayTotalMPG();
     }
 }
